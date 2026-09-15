80a43ff95b763a04d297afdb126948a60d7826f1
s-primarycode <154287418+F-StevenM@users.noreply.github.com>
2026-09-14 19:12:04 -0700

    Finish Level 2 and 3 challenges

---
+++ day_1/helloworld.py
@@ -0,0 +1,25 @@
+# Exercise: Level 2
+print(3 + 4) # 2
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+print('Steve') # 3
+print('N/A')
+print('USA')
+print('I am enjoying 30 days of python')
+
+print(type(10)) # 4
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Steve'))
+print(type('N/A'))
+print(type('USA'))
+
+# Exercise: Level 3
+print('√',((2-10)**2 + (3-8)**2))
