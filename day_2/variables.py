96a75f9505e6647fd5ce012772dd1e89352a5eea
s-primarycode <154287418+F-StevenM@users.noreply.github.com>
2026-09-15 16:01:39 -0700

    Finish Day 2

---
+++ day_2/variables.py
@@ -0,0 +1,58 @@
+# Exercises: Level 1
+# Day 2: 30 Days of python programming
+
+first_name = 'Steven'
+last_name = 'N/A'
+full_name = 'Steven N/A'
+country = 'United States'
+city = 'Los Angeles'
+age = 22
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+info = (first_name, last_name, city, country)
+
+# Exercises: Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(info))
+
+print(len(first_name))
+num_one = 5
+num_two = 4
+num_three = num_one + num_two
+num_four = num_two - num_one
+num_five = num_two * num_one
+num_six = num_one / num_two
+num_seven = num_two % num_one
+num_eight = num_one ** num_two
+num_nine = num_one // num_two
+print(num_three)
+print(num_four)
+print(num_five)
+print(num_six)
+print(num_seven)
+print(num_eight)
+print(num_nine)
+area_of_circle = 2826
+circum_of_circle = '188.4'
+radius = int(input('Radius: '))
+result = 3.14 * radius **2
+print(result)
+first_name = input('What is your first name?: ')
+last_name = input('What is your last name?: ')
+country = input('WHat country do you live in?: ')
+age = input('How old are you?: ')
+print(first_name)
+print(last_name)
+print(country)
+print(age)
