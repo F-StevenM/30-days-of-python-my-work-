99c5e38fb7f06112fb3869839ea7f87497996370
user1 <154287418+F-StevenM@users.noreply.github.com>
2026-09-16 15:47:36 -0700

    Finish day 3 part 1

---
+++ day_3/operators.py
@@ -0,0 +1,52 @@
+# Exercsies - Day 3
+# 1 - 3
+age = 22
+my_height = 5.4
+complex_num = 0.075
+#print(age)
+#print(my_height)
+#print(complex_num)
+
+# 4
+#base = int(input('Enter base: '))
+#height = int(input('Enter base: '))
+#area = 0.5 * base * height
+#print('The area of the triangle is', area)
+
+# 5
+#side_a = int(input('Enter side a: '))
+#side_b = int(input('Enter side b: '))
+#side_c = int(input('Enter side c: '))
+#perimeter = side_a + side_b + side_c
+#print('The perimeter of the triangle is', perimeter)
+
+# 6
+#length = int(input('Enter length: '))
+#width = int(input('Enter width: '))
+#area = length * width
+#perimeter = 2 * (length + width)
+#print('The area of the rectangle is', area)
+#print('The perimeter of the rectangle is', perimeter)
+
+# 7 
+#radius = int(input('Enter the radius: '))
+#area = 3.14 * radius * radius
+#circumference = 2 * 3.14 * radius
+#print('The area of the circle is', area)
+#print('The circumference of the circle is', circumference)
+
+# 8
+#x_and_y = (1, 0, 0, -2)
+slope_1 = (-2 - 0) / (0 - 1)
+#print('The slope is:', slope_1)
+
+# 9
+#x_and_y = (2, 2, 6, 10)
+slope_2 = (10 - 2) / (6 - 2)
+#d_1 = (2 - 6)**2 + (10 - 2)**2
+#d_2 = d_1 ** 0.5
+#print('The slope is:', slope_2)
+#print('The Euclidean distance is:', d_2)
+
+# 10
+print(slope_1 and slope_2)
