757bca8c702579fb2b0d5b5313077320c44c607e
user1 <154287418+F-StevenM@users.noreply.github.com>
2026-09-17 18:13:24 -0700

    Complete day 3 part 2

--- day_3/operators.py
+++ day_3/operators.py
@@ -37,16 +37,79 @@ complex_num = 0.075
 
 # 8
 #x_and_y = (1, 0, 0, -2)
-slope_1 = (-2 - 0) / (0 - 1)
+#slope_1 = (-2 - 0) / (0 - 1)
 #print('The slope is:', slope_1)
 
 # 9
 #x_and_y = (2, 2, 6, 10)
-slope_2 = (10 - 2) / (6 - 2)
+#slope_2 = (10 - 2) / (6 - 2)
 #d_1 = (2 - 6)**2 + (10 - 2)**2
 #d_2 = d_1 ** 0.5
 #print('The slope is:', slope_2)
 #print('The Euclidean distance is:', d_2)
 
 # 10
-print(slope_1 and slope_2)
+#print(slope_1, ",", slope_2)
+
+# 11
+#x_value = int(input('Enter the x value: '))
+#y_value = x_value **2 + 6 * x_value + 9
+#print(y_value)
+
+# 12
+#print(len('Python') and len('Dragon'))
+
+# 13
+#print('on in Python and Dragon', 'on' in 'Python', 'on' in 'Dragon')
+
+# 14
+#print('jargon' in 'I hope this course is not full of jargon')
+
+# 15
+#print('on in Python and Dragon', 'on' not in 'Python', 'on' not in 'Dragon')
+
+# 16
+#word = len('Python')
+#num = float(word)
+#string = str(num)
+#print(string)
+
+# 17
+#num = int(input('Enter a number: '))
+#divide = num % 2 == 0
+#print(divide)
+
+# 18
+#num1 = int(7 // 3)
+#compare = num1 == 2.7
+#print(compare)
+
+# 19
+#num1 = '10'
+#compare = num1 == 10
+#print(compare)
+
+# 20
+#num1 = int(9.8)
+#compare = num1 == 10
+#print(compare)
+
+# 21
+#hours = int(input('Enter hours: '))
+#rate = int(input('Enter rate per hour: '))
+#weekly_earnings = hours * rate
+#print('Your weekly earning is', weekly_earnings)
+
+# 22
+#years = int(input('Enter number of years you have lived: '))
+#convert = years * 31557600
+#print('You have lived for', convert, 'seconds')
+
+# 23
+#print(1, 1, 1, 1, 1)
+#print(2, 1, 2, 4, 8)
+#print(3, 1, 3, 9, 27)
+#print(4, 1, 4, 16, 64)
+#print(5, 1, 5, 25, 125)
+
+# ending here as to not end on 113
