23e1100195fda82820ce5b722d176d3da5ec1515
user1 <154287418+F-StevenM@users.noreply.github.com>
2026-09-18 16:11:33 -0700

    Create strings.py

---
+++ day_4/strings.py
@@ -0,0 +1,4 @@
+multiline_string = '''I am a teacher and enjoy teaching.
+I didn't find anything as rewarding as empowering people.
+That is why I created 30 days of python.'''
+print(multiline_string)
